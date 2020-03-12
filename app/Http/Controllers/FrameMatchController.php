2cbb6ee76bef9102c27425457d7f054dc513fcd0
udithj <jayasundara.udith@gmail.com>
2020-03-12 15:53:04 +0530

    Create FrameMatch Model and resources

---
+++ app/Http/Controllers/FrameMatchController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FrameMatch;
+use Illuminate\Http\Request;
+
+class FrameMatchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\FrameMatch  $frameMatch
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FrameMatch $frameMatch)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\FrameMatch  $frameMatch
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FrameMatch $frameMatch)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\FrameMatch  $frameMatch
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FrameMatch $frameMatch)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\FrameMatch  $frameMatch
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FrameMatch $frameMatch)
+    {
+        //
+    }
+}
---
+++ database/factories/FrameMatchFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\FrameMatch;
+use Faker\Generator as Faker;
+
+$factory->define(FrameMatch::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
