2499a24f2121859a31ae2437c18519d90cab5c04
udithj <jayasundara.udith@gmail.com>
2020-03-12 15:50:15 +0530

    Create ChannelFee Model and resources

---
+++ app/ChannelFee.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ChannelFee extends Model
+{
+    //
+}
---
+++ database/factories/ChannelFeeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ChannelFee;
+use Faker\Generator as Faker;
+
+$factory->define(ChannelFee::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
