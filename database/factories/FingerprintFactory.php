7ce0ad59bbe93afa99dfb851a4bc01109cd8319b
udithj <jayasundara.udith@gmail.com>
2020-03-12 15:51:14 +0530

    Create Fingerprint Model and resources

---
+++ database/factories/FingerprintFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Fingerprint;
+use Faker\Generator as Faker;
+
+$factory->define(Fingerprint::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
