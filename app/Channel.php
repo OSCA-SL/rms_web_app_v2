8a64175ccf2943a82c5dec8bc5ee1a9f582019b2
udithj <jayasundara.udith@gmail.com>
2020-03-19 07:02:40 +0530

    Setup Channel Relationships and migration

--- app/Channel.php
+++ app/Channel.php
@@ -3,8 +3,203 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Channel extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * Get the matches associated with the channel.
+     */
+    public function matches()
+    {
+        return $this->hasMany('App\Models\Match', 'song_id');
+    }
+
+    /**
+     * Get the contact user associated with the channel.
+     */
+    public function contactUser(){
+        return $this->belongsTo('App\Models\User', 'contact_user');
+    }
+
+    /**
+     * Get the fees associated with the channel.
+     */
+    public function fee()
+    {
+        return $this->hasMany('App\Models\Fee', 'channel_id');
+    }
+
+    /**
+     * Get the user who added the channel.
+     */
+    public function addedUser(){
+        return $this->belongsTo('App\Models\User', 'added_by');
+    }
+
+    /*
+    * Methods for file processing
+    * */
+
+    public function isFirstClipFailed()
+    {
+        return $this->fetch_status <= 0;
+    }
+
+    public function setFirstClipFailed()
+    {
+        $this->attributes['fetch_status'] = 0;
+        $this->fetch_status = 0;
+        return 0;
+    }
+
+    public function isFirstClipOk()
+    {
+        return $this->fetch_status >= 1;
+    }
+
+    public function setFirstClipOk()
+    {
+        $this->attributes['fetch_status'] = 1;
+        $this->fetch_status = 1;
+        return 1;
+    }
+
+    public function isSecondClipFailed()
+    {
+        return $this->fetch_status <= 2;
+    }
+
+    public function setSecondClipFailed()
+    {
+        $this->attributes['fetch_status'] = 2;
+        $this->fetch_status = 2;
+        return 2;
+    }
+
+    public function isSecondClipOk()
+    {
+        return $this->fetch_status >= 3;
+    }
+
+    public function setSecondClipOk()
+    {
+        $this->attributes['fetch_status'] = 3;
+        $this->fetch_status = 3;
+        return 3;
+    }
+
+
+    public function isMergingFailed()
+    {
+        return $this->fetch_status <= 4;
+    }
+
+    public function setMergingFailed()
+    {
+        $this->attributes['fetch_status'] = 4;
+        $this->fetch_status = 4;
+        return 4;
+    }
+
+    public function isMergingOk()
+    {
+        return $this->fetch_status >= 5;
+    }
+
+    public function setMergingOk()
+    {
+        $this->attributes['fetch_status'] = 5;
+        $this->fetch_status = 5;
+        return 5;
+    }
+
+    public function isMatchRequestFailed()
+    {
+        return $this->fetch_status <= 6;
+    }
+
+    public function setMatchRequestFailed()
+    {
+        $this->attributes['fetch_status'] = 6;
+        $this->fetch_status = 6;
+        return 6;
+    }
+
+    public function isMatchRequestOk()
+    {
+        return $this->fetch_status >= 7;
+    }
+
+    public function setMatchRequestOk()
+    {
+        $this->attributes['fetch_status'] = 7;
+        $this->fetch_status = 7;
+        return 7;
+    }
+
+    public function getPreviousFetch()
+    {
+        $day = $this->fetched_day == null?0:$this->fetched_day;
+        $hour = $this->fetched_hour == null?0:$this->fetched_hour;
+        $minute = $this->fetched_minute == null?0:$this->fetched_minute;
+
+        if ($minute <= 0){
+            if ($hour <= 0){
+                $day = ($day + 59) % 60;
+            }
+            $hour = ($hour + 23) % 24;
+        }
+
+        $minute = ($minute + 59) % 60;
+
+        return ['day' => $day, 'hour' => $hour, 'minute' => $minute];
+
+    }
+
+    public function getCurrentFetch()
+    {
+        $day = $this->fetched_day == null?0:$this->fetched_day;
+        $hour = $this->fetched_hour == null?0:$this->fetched_hour;
+        $minute = $this->fetched_minute == null?0:$this->fetched_minute;
+
+        return ['day' => $day, 'hour' => $hour, 'minute' => $minute];
+
+    }
+
+    public function getNextFetch()
+    {
+        $day = $this->fetched_day == null?0:$this->fetched_day;
+        $hour = $this->fetched_hour == null?0:$this->fetched_hour;
+        $minute = $this->fetched_minute == null?0:$this->fetched_minute;
+
+        if ($minute >= 59){
+            if ($hour >= 23){
+                $day = ($day + 1) % 60;
+            }
+
+            $hour = ($hour + 1) % 24;
+        }
+
+        $minute = ($minute + 1) % 60;
+
+        return ['day' => $day, 'hour' => $hour, 'minute' => $minute];
+    }
+
+    public function setFetched($fetch)
+    {
+        $this->fetched_day = $fetch['day'];
+        $this->fetched_hour = $fetch['hour'];
+        $this->fetched_minute = $fetch['minute'];
+        $this->save();
+    }
 }
--- database/migrations/2020_03_12_101854_create_channels_table.php
+++ database/migrations/2020_03_12_101854_create_channels_table.php
@@ -14,8 +14,46 @@ class CreateChannelsTable extends Migration
     public function up()
     {
         Schema::create('channels', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('logger');
+            $table->decimal('frequency', 8, 4); // in MHz
+            $table->unsignedBigInteger('contact_user');
+            $table->unsignedBigInteger('added_by');
+
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->string('details')->nullable();
+
+            /*Used for fetching from the Radio Loging device*/
+            $table->dateTime('last_fetch_at')->nullable();
+            $table->dateTime('aired_time')->nullable();
+            $table->unsignedTinyInteger('fetched_day')->nullable();
+            $table->unsignedTinyInteger('fetched_hour')->nullable();
+            $table->unsignedTinyInteger('fetched_minute')->nullable();
+            $table->tinyInteger('fetch_status')->default(0);
+            /*
+             * 0 - first clip failed
+             * 1 - first clip ok
+             * 2 - second clip failed
+             * 3 - second clip ok
+             * 4 - merging failed
+             * 5 - merging ok
+             * 6 - Match Request Failed
+             * 7 - Match Request Ok
+             * */
+            /*$table->string('first_clip')->nullable();
+            $table->string('second_clip')->nullable();*/
+            $table->string('clip_path')->nullable();
+
+            /*Used only when matching with songs. Used to keep track of the gaps*/
+            $table->bigInteger('song_id')->nullable();
+            $table->bigInteger('match_id')->nullable();
+            $table->tinyInteger('gaps')->nullable();
+            /*Keep track of gaps*/
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
